c9463cee67948af4487d854bd9c600fc0e3c2867
Paula Cano <paulacanoroman@gmail.com>
2023-12-16 16:07:00 +0100

    feat: create component

---
+++ components/UseformExample.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UseformExample = () => {
+  return (
+    <div>U</div>
+  )
+}
+
+export default UseformExample
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
+import UseformExample from '@/components/useformExample'
 
 
 export default function Home() {
@@ -12,7 +13,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main}`}>
-       
+       <UseformExample></UseformExample>
       </main>
     </>
   )
