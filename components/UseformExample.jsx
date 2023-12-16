32aff185c57f6d43695cbcaca9d26312262898c5
Paula Cano <paulacanoroman@gmail.com>
2023-12-16 16:23:16 +0100

    feat: form with inputs and labels

--- components/UseformExample.jsx
+++ components/UseformExample.jsx
@@ -9,7 +9,26 @@ const UseformExample = () => {
     <>
     <h2>Formulario con useForm</h2>
     <form>
-      
+    <div>
+        <label>Nombre</label>
+        <input type='text'></input>
+      </div>
+      <div>
+        <label>Dirección</label>
+        <input type='text'></input>
+      </div>
+      <div>
+        <label>Edad</label>
+        <input type='text'></input>
+      </div>
+      <div>
+        <label>País</label>
+        <select>
+          <option value="es">España</option>
+          <option value="pr">Portugal</option>
+          <option value="fr">Francia</option>
+        </select>
+      </div>
     </form>
     </>
   )
